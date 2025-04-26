6aee49c0a125413e12ce66a9e84cb83adbea645d
Pavit Lertpiromlak <i752coffee@gmail.com>
2025-04-27 00:29:58 +0700

    Update SciencePlanController.java
    changed to requestParam Integer + check if planId == null  + reformat

--- gemini/backend/src/main/java/ict/mahidol/gemini/controller/SciencePlanController.java
+++ gemini/backend/src/main/java/ict/mahidol/gemini/controller/SciencePlanController.java
@@ -132,12 +132,14 @@ public class SciencePlanController {
     }
 
     @CrossOrigin
-    @DeleteMapping("/delete/{planId}")
+    @DeleteMapping("/delete")
     public @ResponseBody ResponseEntity<Map<String, String>> deleteSciencePlanById(
-            @PathVariable("planId") int planId,
+            @RequestParam(value = "planId", required = false) Integer planId,
             HttpServletRequest request) {
                 
-        // Springboot automatically handles planId = null 
+        if (planId == null) {
+            return new ResponseEntity<>(Map.of("message", "Missing required parameter"), HttpStatus.BAD_REQUEST);
+        }
         
         Claims claims = (Claims) request.getAttribute("claims");
         if (claims == null) {
@@ -146,13 +148,11 @@ public class SciencePlanController {
         }
 
         String role = claims.get("role", String.class);
-
         if (role == null || !role.equals("astronomer")) {
             return new ResponseEntity<>(Map.of("message", "Access denied"), HttpStatus.FORBIDDEN);
         }
 
         Optional<SciencePlan> planOptional = sciencePlanRepository.findById(planId);
-
         if (planOptional.isPresent()) {
             sciencePlanRepository.deleteById(planId);
             return new ResponseEntity<>(Map.of("message", "Successfully deleted science plan"), HttpStatus.OK);
