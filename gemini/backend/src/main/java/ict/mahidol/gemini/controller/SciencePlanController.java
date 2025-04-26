32c39e55278f3bece2a98067e05c9d1ccd9c5a06
DEVINPL <107537174+DEVN-P@users.noreply.github.com>
2025-04-26 22:57:02 +0700

    feat: /submit endpoint

--- gemini/backend/src/main/java/ict/mahidol/gemini/controller/SciencePlanController.java
+++ gemini/backend/src/main/java/ict/mahidol/gemini/controller/SciencePlanController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,16 +37,19 @@ public class SciencePlanController {
     public @ResponseBody ResponseEntity<Map<String, String>> createSciencePlan(
             @RequestBody SciencePlanDto sciencePlan, HttpServletRequest request) {
 
+        // Handle missing authorization header
+        if (request.getHeader("Authorization") == null) {
+            return ResponseEntity.badRequest().body(Map.of("message", "There's no authorization header attached"));
+        }
+
         // Accessing JWT-authenticated user info
         Claims claims = (Claims) request.getAttribute("claims");
         String username = claims.get("username", String.class);
         String role = claims.get("role", String.class);
-        // return ResponseEntity.ok(Map.of("message", "Found user: " + username + " with
-        // role: " + role));
 
-        // Handle missing authorization header
-        if (request.getHeader("Authorization") == null) {
-            return ResponseEntity.badRequest().body(Map.of("message", "Missing authorization header"));
+        // Handle unauthorized access
+        if (!role.equals("astronomer")) {
+            return ResponseEntity.status(403).body(Map.of("message", "Access denied"));
         }
 
         String planName = sciencePlan.getPlanName();
@@ -57,14 +62,10 @@ public class SciencePlanController {
         DataProcessingRequirement dataProcessingRequirement = sciencePlan.getDataProcessingReq();
 
         // Handle missing fields
-        if (planName == null || funding == null || objective == null || starSystem == null || startDate == null
+        if (planName == null || funding == null || funding == -1 || objective == null || starSystem == null
+                || startDate == null
                 || endDate == null || telescopeLocation == null || dataProcessingRequirement == null) {
-            return ResponseEntity.badRequest().body(Map.of("message", "Missing required parameter"));
-        }
-
-        // Handle unauthorized access
-        if (!role.equals("astronomer")) {
-            return ResponseEntity.badRequest().body(Map.of("message", "Access denied"));
+            return ResponseEntity.badRequest().body(Map.of("message", "Missing/Invalid parameters"));
         }
 
         // Fetch the first and last name using the username
@@ -83,6 +84,62 @@ public class SciencePlanController {
         sciencePlanRepository.save(newSciencePlan);
 
         return ResponseEntity.ok(Map.of("message", "Science plan created successfully"));
+    }
 
+    @CrossOrigin
+    @PutMapping("/submit")
+    public ResponseEntity<Map<String, String>> invalidSubmission1() {
+        return ResponseEntity.badRequest().body(Map.of("message", "Missing/Invalid parameters"));
+    }
+
+    @CrossOrigin
+    @PutMapping("/submit/")
+    public ResponseEntity<Map<String, String>> invalidSubmission2() {
+        return ResponseEntity.badRequest().body(Map.of("message", "Missing/Invalid parameters"));
+    }
+
+    @CrossOrigin
+    @PutMapping("/submit/{planId}")
+    public ResponseEntity<Map<String, String>> submitSciencePlan(@PathVariable int planId,
+            HttpServletRequest request) {
+
+        // Handle missing authorization header
+        if (request.getHeader("Authorization") == null) {
+            return ResponseEntity.badRequest().body(Map.of("message", "There's no authorization header attached"));
+        }
+
+        // Accessing JWT-authenticated user info
+        Claims claims = (Claims) request.getAttribute("claims");
+        String username = claims.get("username", String.class);
+        String role = claims.get("role", String.class);
+
+        // Handle unauthorized access
+        if (!role.equals("astronomer")) {
+            return ResponseEntity.status(403).body(Map.of("message", "Access denied"));
+        }
+
+        // Fetch the first and last name using the username
+        Optional<User> user = userRepository.findByUsername(username);
+        String firstName = user.get().getFirstName();
+        String lastName = user.get().getLastName();
+        String submitter = firstName + " " + lastName;
+
+        // Find the science plan by ID
+        Optional<SciencePlan> sciencePlan = sciencePlanRepository.findById(planId);
+
+        // Handle science plan not found
+        if (!sciencePlan.isPresent()) {
+            return ResponseEntity.status(404)
+                    .body(Map.of("message", "No science plan record with id: " + planId + " found"));
+        }
+
+        // Update the science plan status and submitter
+        sciencePlan.get().setPlanStatus("submitted");
+        sciencePlan.get().setSubmitter(submitter);
+
+        // Save the updated science plan to the database
+        sciencePlanRepository.save(sciencePlan.get());
+
+        return ResponseEntity.ok(Map.of("message", "Suceessfully submitted science plan."));
     }
 }
