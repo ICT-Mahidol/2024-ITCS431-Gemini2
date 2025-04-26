2ddd070ac2e4ce2edc87cad5d970d26ccd46598f
S4NHXNU1 <93254994+S4NHXNU1@users.noreply.github.com>
2025-04-26 11:31:58 +0700

    Resolve Feature/GEM-0 Initalize Science Plan Model (#29)
    * feat: GEM-0 add sciencePlan model + repository + dataProcessingReq model
    
    * fix: GEM-0 update gitignore
    
    * fix: GEM-0 update gitignore + remove db cache

---
+++ gemini/backend/src/main/java/ict/mahidol/gemini/repository/SciencePlanRepository.java
@@ -0,0 +1,9 @@
+package ict.mahidol.gemini.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ict.mahidol.gemini.model.SciencePlan;
+
+public interface SciencePlanRepository extends CrudRepository<SciencePlan, Integer> {
+    
+}
