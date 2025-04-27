d009fbcb74028332e1c2012f4f8426997a5e9c32
S4NHXNU1 <sanhanut2003@gmail.com>
2025-04-27 14:47:22 +0700

    feat: GEM-1 add science plan list endpoint + update repository

--- gemini/backend/src/main/java/ict/mahidol/gemini/repository/SciencePlanRepository.java
+++ gemini/backend/src/main/java/ict/mahidol/gemini/repository/SciencePlanRepository.java
@@ -1,9 +1,12 @@
 package ict.mahidol.gemini.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ict.mahidol.gemini.model.SciencePlan;
 
 public interface SciencePlanRepository extends CrudRepository<SciencePlan, Integer> {
     
+    public List<SciencePlan> findByPlanStatus(String planStatus);
 }
