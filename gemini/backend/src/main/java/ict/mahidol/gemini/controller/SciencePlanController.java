d9c8e5e617191645e5499401b7828e8b930ed46a
S4NHXNU1 <93254994+S4NHXNU1@users.noreply.github.com>
2025-04-27 17:17:37 +0700

    refactor: GEM-0 add services layer + modify controller (#36)

--- gemini/backend/src/main/java/ict/mahidol/gemini/controller/SciencePlanController.java
+++ gemini/backend/src/main/java/ict/mahidol/gemini/controller/SciencePlanController.java
@@ -1,8 +1,5 @@
 package ict.mahidol.gemini.controller;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -74,29 +71,12 @@ public class SciencePlanController {
     @CrossOrigin
     @PutMapping("/test")
     public @ResponseBody
-    ResponseEntity<Map<String, String>> testSciencePlan(@RequestParam("planId") Integer planId, HttpServletRequest request) 
+    ResponseEntity<Map<String, String>> testSciencePlan(@RequestParam(value = "planId", required = false) Integer planId, HttpServletRequest request) 
     {
         Claims claims = (Claims) request.getAttribute("claims");
         String role = claims.get("role", String.class);
-        if (!role.equals("astronomer")) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "Access denied"));
-        }
-
-        if (planId == null) {
-            return ResponseEntity.badRequest().body(Map.of("message", "Missing/Invalid parameters"));
-        }
-
-        Optional<SciencePlan> sciencePlan = sciencePlanRepository.findById(planId);
-
-        if (!sciencePlan.isPresent()) {
-            return ResponseEntity.status(404)
-                    .body(Map.of("message", "No science plan record with id: " + planId + " found"));
-        }
-
-        sciencePlan.get().setPlanStatus("TESTED");
-        sciencePlanRepository.save(sciencePlan.get());
-
-        return ResponseEntity.ok(Map.of("message", "successfully tested science plan"));
+        
+        return sciencePlanServices.TestSciencePlan(role, planId);
     }
 
     @CrossOrigin
@@ -123,43 +103,7 @@ public class SciencePlanController {
     @GetMapping("/list")
     public @ResponseBody ResponseEntity<List<Map<String, Object>>> getSciencePlanList(@RequestParam(value = "status", required = false) String status,
     HttpServletRequest request) {
-        List<Map<String, Object>> planList = new ArrayList<>();
-
-        if(status == null || "".equals(status))
-        {
-            Iterable<SciencePlan> plans = sciencePlanRepository.findAll();
-
-            if (!plans.iterator().hasNext()) {
-                return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NOT_FOUND);
-            }
-            else {
-                for (SciencePlan plan : plans) {
-                    Map<String, Object> planMap = new HashMap<>();
-                    planMap.put("planId", plan.getPlanId());
-                    planMap.put("planName", plan.getPlanName());
-                    planMap.put("planStatus", plan.getPlanStatus());
-                    planList.add(planMap);
-                }
-                return ResponseEntity.ok(planList);
-            }
-        }
-        else
-        {
-            List<SciencePlan> plans = sciencePlanRepository.findByPlanStatus(status);
-
-            if (plans.isEmpty()) {
-                return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NOT_FOUND);
-            } else {
-                // Build a simple map from your results
-                for (SciencePlan plan : plans) {
-                    Map<String, Object> planMap = new HashMap<>();
-                    planMap.put("planId", plan.getPlanId());
-                    planMap.put("planName", plan.getPlanName());
-                    planMap.put("planStatus", plan.getPlanStatus());
-                    planList.add(planMap);
-                }
-                return ResponseEntity.ok(planList);
-            }
-        }
+        if(status == null || "".equals(status)) return sciencePlanServices.GetSciencePlanList();
+        else return sciencePlanServices.GetSciencePlanListByStatus(status);
     }
 }
